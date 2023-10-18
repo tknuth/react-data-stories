3faf9bc864f4de45b6f9b1002a66790243d85270
Tobias Knuth <tknuth@users.noreply.github.com>
2023-10-18 16:12:50 +0200

    added gatsby app and storybook

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `React Data Stories`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
